c9c2f3ab81a4abeaef81ac54e0cf5ec0b958195d
Ali Amrahli <ali.emrahli@gmail.com>
2023-11-30 02:17:12 +0400

    Adding diplay feature. Currently not working; need to be fixed

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const endpoint = 'https://dummyjson.com/products';
 
-
 const fetchData = async () => {
   try {
     const response = await fetch(endpoint);
@@ -10,11 +9,27 @@ const fetchData = async () => {
     }
 
     const data = await response.json();
-    console.log('Fetched data:', data);
+    displayData(data); // Call the function to display data
   } catch (error) {
     console.error(`Error fetching data: ${error.message}`);
   }
 };
 
+const displayData = (data) => {
+  const productList = document.getElementById('productList');
+
+  data.forEach((product) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      <strong>Title:</strong> ${product.title}<br>
+      <strong>Price:</strong> ${product.price}<br>
+      <strong>Discount:</strong> ${product.discount}<br>
+      <strong>Category:</strong> ${product.category}<br>
+      <strong>Stock:</strong> ${product.stock}<br>
+      <img src="${product.thumbnail}" alt="Thumbnail"><br><br>
+    `;
+    productList.appendChild(listItem);
+  });
+};
 
 fetchData();
