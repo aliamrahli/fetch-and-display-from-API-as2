e1b64e83999d27f045aa38886bc87d988657a651
Ali Amrahli <ali.emrahli@gmail.com>
2023-12-01 23:58:56 +0400

    Display data fixed. Products are shown on the page

--- app.js
+++ app.js
@@ -9,26 +9,32 @@ const handleFetchErrors = (response) => {
 };
 
 
-const displayProducts = (products) => {
+const displayProducts = (data) => {
   const productContainer = document.getElementById('productContainer');
+  const products = data.products;
 
-  products.forEach((product) => {
-    const productDiv = document.createElement('div');
-    productDiv.classList.add('product');
+  // Check if products is an array
+  if (Array.isArray(products)) {
+    products.forEach((product) => {
+      const productDiv = document.createElement('div');
+      productDiv.classList.add('product');
 
-    // Display product details
-    productDiv.innerHTML = `
-      <h2>${product.title}</h2>
-      <p>Price: $${product.price}</p>
-      <p>Discount: ${product.discount}%</p>
-      <p>Category: ${product.category}</p>
-      <p>Stock: ${product.stock}</p>
-      <img src="${product.thumbnail}" alt="${product.title} thumbnail">
-    `;
+      // Display product details
+      productDiv.innerHTML = `
+        <h2>${product.title}</h2>
+        <p>Price: $${product.price}</p>
+        <p>Discount: ${product.discount}%</p>
+        <p>Category: ${product.category}</p>
+        <p>Stock: ${product.stock}</p>
+        <img src="${product.thumbnail}" alt="${product.title} thumbnail">
+      `;
 
-    // Append the product div to the container
-    productContainer.appendChild(productDiv);
-  });
+      // Append the product div to the container
+      productContainer.appendChild(productDiv);
+    });
+  } else {
+    console.error('Invalid data format. Expected an array of products.');
+  }
 };
 
 // Fetch data from the /products endpoint
@@ -36,7 +42,8 @@ fetch(endpoint)
   .then(handleFetchErrors)
   .then((data) => {
     // Handle the data received from the server
-    console.log('Products data:', data);
+    displayProducts(data);
+    console.log(data)
   })
   .catch((error) => {
     // Handle any errors that occurred during the fetch operation
