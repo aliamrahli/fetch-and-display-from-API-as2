817b479470caaafe6c8243256ac9da6e9e1e3367
Ali Amrahli <ali.emrahli@gmail.com>
2023-12-03 23:06:03 +0400

    Searching added. Need to be fixed

--- app.js
+++ app.js
@@ -14,29 +14,64 @@ const displayProducts = (data) => {
   
   const products = data.products;
 
+
+  // Populate category select options
+  // let categories = [...new Set(products.map(product => product.category))];
+  // categories.forEach(category => {
+  //     const option = document.createElement('option');
+  //     option.value = category;
+  //     option.textContent = category;
+  //     categorySelect.appendChild(option);
+  // });
+
+  
+
   // Check if products is an array
-  if (Array.isArray(products)) {
-    products.forEach((product) => {
-      const productDiv = document.createElement('div');
-      productDiv.classList.add('product');
+    if (Array.isArray(products)) {
+      products.forEach((product) => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product');
 
-      // Display product details
-      productDiv.innerHTML = `
-        <h2>${product.title}</h2>
-        <p>Price: $${product.price}</p>
-        <p>Discount: ${product.discountPercentage}%</p>
-        <p>Category: ${product.category}</p>
-        <p>Stock: ${product.stock}</p>
-        <a href="product_info.html?id=${product.id}">
-          <img src="${product.thumbnail}" alt="${product.title} thumbnail">
-        </a>
-      `;
+        // Display product details
+        productDiv.innerHTML = `
+          <h2>${product.title}</h2>
+          <p>Price: $${product.price}</p>
+          <p>Discount: ${product.discountPercentage}%</p>
+          <p>Category: ${product.category}</p>
+          <p>Stock: ${product.stock}</p>
+          <a href="product_info.html?id=${product.id}">
+            <img src="${product.thumbnail}" alt="${product.title} thumbnail">
+          </a>
+        `;
 
-      // Append the product div to the container
-      productContainer.appendChild(productDiv);
-    });
+        // Append the product div to the container
+        productContainer.appendChild(productDiv);
+      });
+    } else {
+      console.error('Invalid data format. Expected an array of products.');
+    }
+};
+
+
+const searchProducts = () => {
+  const searchInput = document.getElementById('searchInput');
+  const keyword = searchInput.value.trim();
+
+  // Check if a keyword is provided
+  if (keyword !== '') {
+    const searchEndpoint = `https://dummyjson.com/products/search?q=phone`;
+
+    fetch(searchEndpoint)
+      .then(handleFetchErrors)
+      .then((data) => {
+        // Handle the data received from the server
+        displayProducts(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching search results:', error.message);
+      });
   } else {
-    console.error('Invalid data format. Expected an array of products.');
+    console.error('Please enter a keyword to search.');
   }
 };
 
