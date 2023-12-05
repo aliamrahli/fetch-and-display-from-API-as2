d42a590a493de24fd5baf118169a544aaa3eb88d
Ali Amrahli <ali.emrahli@gmail.com>
2023-12-05 19:45:22 +0400

    All searching bugs fixed. Dynamic pagination mechanism added. You can search among the products located in one particular page. Not found feature added.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let products;
 let allCategories;
 let productDiv;
 let productContainer;
+let pageNumber=1;
 
 const clearInput = () => {
   document.getElementById('searchInput').value = '';
@@ -31,10 +32,10 @@ const storeCategories = (data) => {
   }
     
 
-const searchProducts = () => {
+const searchProducts = (data) => {
   let searchInput = document.getElementById("searchInput")
   let filter = searchInput.value.toLowerCase()
-  let apiURL = "https://dummyjson.com/products/search?q=" + encodeURIComponent(filter)
+  let apiURL = "https://dummyjson.com/products/search?q=" + encodeURIComponent(filter)+"&limit=100"
   fetch(apiURL)
     .then(handleFetchErrors)
     .then((data)=> {
@@ -48,30 +49,43 @@ const searchProducts = () => {
 
 const filterProducts = (data) => {
   let categorySelect = document.getElementById("categorySelect");
+  min = (pageNumber-1)*10 + 1
+  max = (pageNumber)*10
   selectedCategory = categorySelect.value.toLowerCase();
   productContainer = document.getElementById('productContainer');
   productContainer.innerHTML = ""
   products = data.products;
+  
   products.forEach((product) => {
     const productDiv = document.createElement('div');
     productDiv.classList.add('product');
-    if(selectedCategory=="all" || product.category == selectedCategory) {
+    if(product.id >= min && product.id <=max) {
+      if(selectedCategory=="all" || product.category == selectedCategory) {
 
-      productDiv.innerHTML = `
-      <a href="product_info.html?id=${product.id}">
-          <h2>${product.title}</h2>
-          <p class="description">Description: ${product.description}</p>
-          <p>Price: $${product.price}</p>
-          <p>Discount: ${product.discountPercentage}%</p>
-          <p class="category">Category: ${product.category}</p>
-          <p>Stock: ${product.stock}</p>
-          <img src="${product.thumbnail}" alt="${product.title} thumbnail">
-      </a>
-    `;
-
-    productContainer.appendChild(productDiv);
+        productDiv.innerHTML = `
+        <a href="product_info.html?id=${product.id}">
+            <h2>${product.title}</h2>
+            <p class="description">Description: ${product.description}</p>
+            <p>Price: $${product.price}</p>
+            <p>Discount: ${product.discountPercentage}%</p>
+            <p class="category">Category: ${product.category}</p>
+            <p>Stock: ${product.stock}</p>
+            <img src="${product.thumbnail}" alt="${product.title} thumbnail">
+        </a>
+      `;
+  
+      productContainer.appendChild(productDiv);
+      }
     }
-  });
+  })
+  
+        if(productContainer.innerHTML=='') {
+          const h1 = document.createElement('h1')
+          h1.id = "text"
+          productContainer.appendChild(h1)
+          document.getElementById('text').innerHTML = "Not found any products"
+      }
+  
 }
 
 const displayProductInfo = (product, container) => {
@@ -79,11 +93,46 @@ const displayProductInfo = (product, container) => {
   document.getElementById('productContainer').style.display = 'none';
 };
 
-fetch('https://dummyjson.com/products?limit=100')
+const fetchPages = (event) => {
+  id = event.target.id.match(/\d+/)[0]
+  pageNumber = id 
+  // automatically setting the endpoint of fetching. Limit will stay same.
+  // skip will change depending on which page is clicked. Let's say 3rd page clicked, so we
+  // need to skip 20 pages, (id-1)*10 gives us that result for each page
+  fetch(`https://dummyjson.com/products?limit=10&skip=${(id-1)*10}`)
   .then(handleFetchErrors)
   .then((data) => {
     filterProducts(data);
-    console.log(data)
+   
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error.message);
+  });
+
+}
+
+const handlePagination = () => {
+ 
+  // I hardcoded here, because fetching from API doesn't exactly give us the number of products
+  // so we can manually set the number of pages
+  pagebarDiv = document.getElementById("pagebar")
+  for(i=1; i<=10; i++) {
+      let pageButton = document.createElement("button")
+      pageButton.id = `"page${i}"`
+      pageButton.classList.add('pages')
+      pageButton.onclick = fetchPages;
+      pageButton.innerHTML = i
+      pagebarDiv.appendChild(pageButton)
+  }
+  pagebarDiv.style.display = "flex"
+  pagebarDiv.style.justifyContent = "center"
+}
+
+fetch('https://dummyjson.com/products?limit=10')
+  .then(handleFetchErrors)
+  .then((data) => {
+    filterProducts(data);
+   
   })
   .catch((error) => {
     console.error('Error fetching data:', error.message);
@@ -93,6 +142,7 @@ fetch('https://dummyjson.com/products?limit=100')
 .then(handleFetchErrors)
 .then((data)=>{
     storeCategories(data);
+    handlePagination()
 })
 .catch((error) => {
   console.error('Error fetching data:', error.message);
